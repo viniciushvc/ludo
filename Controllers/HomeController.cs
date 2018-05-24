6e0ed3794bb3e4c2e1509a2ec52a76eba83b0504
vinicius vicentini <viniciusvicentini@live.com>
2018-05-24 20:25:05 -0300

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Ludo.Models;
+
+namespace Ludo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Ludo";
+
+            return View();
+        }
+    }
+}
